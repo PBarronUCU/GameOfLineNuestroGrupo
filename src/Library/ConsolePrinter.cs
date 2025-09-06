84fa9f586a516164e01cff49a17e6f94ffc67673
Nico <142958306+NiCo4Lv4r3z@users.noreply.github.com>
2025-09-06 19:57:23 -0300

    Se agrega el código de la clase ConsolePrinter

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+namespace Library;
+
+public class ConsolePrinter //Clase responsable de tomar un array de boleanos e imprimirlo a pantalla
+{
+    //Es la unica responsabilidad que tiene, por lo que solo tendria una razon de cambio
+    public static void Pantalla(bool[,] board)
+    {
+        bool[,] b = board;
+        int width = board.GetLength(0);
+        int height = board.GetLength(1);
+        Console.Clear();
+        StringBuilder s = new StringBuilder();
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (b[x, y])
+                {
+                    s.Append("|X|");
+                }
+                else
+                {
+                    s.Append('_');
+                }
+            }
+
+            s.Append("\n");
+        }
+
+        Console.WriteLine(s.ToString());
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Ucu.Poo.GameOfLife
+namespace Program
 {
     class Program
     {
